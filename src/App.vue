944a7b153dcd5f23c8460d9150470dcbce3317be
azizafarooq <azizafarooq2000@gmail.com>
2024-04-18 19:59:54 +0200

    axios install

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+export default {
+  data() {
+    return {
+      title: "Boolfolio Frontend"
+    }
+  },
+
+  created() {
+    axios.get('http://127.0.0.1:8000/api/projects').then((response) => {
+      this.projects = response.data.data;
+      })
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mt-5">
+    <h1>{{ title }}</h1>
+    <div v-for="project in projects">
+      <ul>
+      <li>Title: {{ project.title }}</li>
+      <li>Description: {{ project.description }}</li>
+    </ul>
+    </div>
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss" scoped>
+@use '/src/scss/general.scss';
 </style>
+
+
+
